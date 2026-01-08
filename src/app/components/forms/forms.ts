141144aa7779f4323252d690c76c7dab4664bf84
Amanda Trinity <amandatrinity16@gmail.com>
2026-01-08 10:53:46 -0300

    feat: handle form submission

--- src/app/components/forms/forms.ts
+++ src/app/components/forms/forms.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [NgOptimizedImage],
+  imports: [
+    NgOptimizedImage,
+    ReactiveFormsModule 
+  ],
   templateUrl: './forms.html',
   styleUrl: './forms.scss',
 })
-export class Forms {
+export class Forms implements OnInit {
   results = [];
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    })
+  }
+
+  onSubmit(): void {
+    console.log('Formulário enviado!', this.form.value);
+  }
+
+  onClean(): void {
+    this.form.reset();
+  }
 }
